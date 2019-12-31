95af66c0f8dafe78cd2a530bd2f240aee1b16fec
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-12-31 15:39:09 -0500

    May have gui by sundown?

--- src/personote/note-view.clj
+++ src/personote/note-view.clj
@@ -1,12 +1,16 @@
 (ns personote.note-view
-    (:require [cljfx.api :as fx])
-    (:import [javafx.scene.input KeyCode KeyEvent]))
+    (:require [fn-fx.fx-dom :as dom]
+              [fn-fx.diff :refer [component defui render should-update?]]
+              [fn-fx.controls :as ui]))
 
-(defn todo-view [{:keys [text id done]}]
-    {:fx/type :h-box
-     :spacing 5
-     :padding 5
-     :children [{:fx/type :check-box
-                 :selected done
-                 :on-selected-changed}]})
-     
+
+(def init-state 
+    {:typed-text ""
+     :notes [
+             {:id 0 :text "Write code." :done false :tags #{"Code"}}
+             {:id 0 :text "Go to a meet-up" :done false :tags #{"Tech" "Code"}}
+             {:id 0 :text "Meditate" :done false :tags #{"Health" "Self-Care"}}
+             {:id 0 :text "Buy Lions Mane" :done false :tags #{"Health"}}]
+     :root-stage? true})
+
+(defonce app-state (atom init-state))
