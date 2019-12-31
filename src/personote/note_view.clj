4333bb00ce2318a21c5d3c89ad1e0e7f39ea26e1
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-12-31 18:22:25 -0500

    can't render app state to ui

--- src/personote/note_view.clj
+++ src/personote/note_view.clj
@@ -6,15 +6,16 @@
 
 
 
-(def init-state 
-    {:typed-text ""
-     :notes [{:id 0 :text "Write code." :done? false :tags #{"Code"}}
-             {:id 0 :text "Go to a meet-up" :done? false :tags #{"Tech" "Code"}}
-             {:id 0 :text "Meditate" :done? false :tags #{"Health" "Self-Care"}}
-             {:id 0 :text "Buy Lions Mane" :done? false :tags #{"Health"}}]
-     :root-stage? true})
+(defonce app-state 
+    (atom 
+     {:typed-text ""
+      :notes [{:id 0 :text "Write code." :done? false :tags #{"Code"}}
+              {:id 0 :text "Go to a meet-up" :done? false :tags #{"Tech" "Code"}}
+              {:id 0 :text "Meditate" :done? false :tags #{"Health" "Self-Care"}}
+              {:id 0 :text "Buy Lions Mane" :done? false :tags #{"Health"}}]
+      :root-stage? false}))
 
-(defonce app-state (atom init-state))
+@app-state
 
 (defn force-exit [root-stage?]
     (reify javafx.event.EventHandler
@@ -25,7 +26,7 @@
 
 (defmulti handle-event (fn [state event] (:event event)))
 
-(defmethod handle-event :reset [_ {:keys [root-stage?]}] (assoc init-state :root-stage? root-stage?))
+; (defmethod handle-event :reset [_ {:keys [root-stage?]}] (assoc state :root-stage? root-stage?))
 (defmethod handle-event :add-item [state {:keys [fn-fx/includes]}] (update-in state :notes conj {:done? false :text (get-in includes [::new-item :text]) :tags #{}}))
 (defmethod handle-event :delete-item [state {:keys [id]}] (update-in state [:notes] 
                                                            (fn [items]
@@ -45,39 +46,35 @@
                                           :on-action {:event :delete-item :id id}))))
 
 (defui MainWindow
-    (render [this {:keys [notes]}]
-        (ui/v-box :style "-fx-base: rgb(30, 30, 35"
+    (render [this {:keys [notes text]}]
+        (ui/v-box :style "-fx-base: rgb(30, 30, 35)"
                   :padding (ui/insets :top-right-bottom-left 25)
                   :children [(ui/text-field :id ::new-item
                                             :prompt-text "What's on your mind?"
                                             :font main-font
                                             :on-action {:event :add-item
-                                                        :fn-fx/include {::new-item #{:text}}})
+                                                        :fn-fx/include {::new-item #{text}}})
                              (ui/v-box :children (map-indexed (fn [id note] (todo-item (assoc note :id id))) notes))])))
                                             
 (defui Stage
-    (render [this {:keys [root-stage? notes typed-text]}]
+    (render [this args]
         (ui/stage :title "Personote"
-                  :on-close-request (force-exit)
                   :min-height 600
                   :listen/height {:event :height-change :fn-fx/include {::new-item #{:text}}}
                   :shown true
-                  :scene (ui/scene :root (ui/border-pane
-                                          :top (ui/h-box
-                                                :padding (javafx.geometry.Insets. 15 12 15 12)
-                                                :spacing 10))))))
+                  :scene (ui/scene :root (main-window args)))))
 
 (defn start!
     ([] (start! {:root-stage? true}))
     ([{:keys [root-stage?]}]
      (swap! app-state assoc :root-stage? root-stage?)
      (let [handler-fn (fn [e] (println e) (try (swap! app-state handle-event e) (catch Throwable exception (println exception))))
-           ui-state (agent (fx-dom/app (ui/stage @app-state) handler-fn))]
+           ui-state (agent (fx-dom/app (stage app-state) handler-fn))]
       (add-watch app-state :ui
         (fn [_ _ _ _]
             (send ui-state (fn [old-ui]
                              (println "-- State Updated --")
-                             (println @app-state)
-                             (fx-dom/update-app old-ui (ui/stage @app-state)))))))))
+                             (println app-state)
+                             (fx-dom/update-app old-ui (stage app-state)))))))))
         
      
