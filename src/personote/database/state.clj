d518b8d62a62860bd9521cb6d65cf64c457808fb
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-01-02 20:32:37 -0500

    User persistence achieved

--- src/personote/database/state.clj
+++ src/personote/database/state.clj
@@ -6,43 +6,34 @@
 (def db-spec
     {:dbtype "postgresql"
      :classname "org.postgresql.Driver"
-     :dbname "personote-db"
-     :user "lorenzo-evans"
-     :port 5432
-     :password "password"})
-; (jdbc/execute! personote (jdbc/drop-table-ddl :users))
-(def user-table-ddl
-        (jdbc/create-table-ddl :users
-                               [[:username "varchar(32)"]
-                                [:email "varchar(32)"]
-                                [:password "varchar(32)"]
-                                [:user_id :serial "PRIMARY KEY"]]))
+     :dbname "personote"
+     
+     :port 5432})
+; (jdbc/execute! db-spec (jdbc/drop-table-ddl :users))
+; (def user-table-ddl
+;         (jdbc/create-table-ddl :users
+;                                [[:username "varchar(32)"]
+;                                 [:email "varchar(32)"]
+;                                 [:password "varchar(32)"]
+;                                 [:user_id :serial "PRIMARY KEY"]
+;                                 [:notes "text[]"]]))
                             
-; (jdbc/execute! personote (jdbc/drop-table-ddl :notes))
-(def note-table-ddl
-        (jdbc/create-table-ddl :notes
-                           [[:title "varchar(32)"]
-                            ["CONSTRAINT id PRIMARY KEY (user_id)"]
-                            [:content :text]
-                            [:tags "text[]"]]))
+; ; (jdbc/execute! db-spec (jdbc/drop-table-ddl :notes))
+; (def note-table-ddl
+;         (jdbc/create-table-ddl :notes
+;                            [[:title "varchar(32)"]
+;                             [:user_id :int "REFERENCES users"]
+;                             [:note_id :serial "PRIMARY KEY"]
+;                             [:content :text]
+;                             [:tags "text[]"]]))
 
-(println "User Table: " user-table-ddl)
-(println "Note Table: " note-table-ddl)
+; (println "User Table: " user-table-ddl)
+; (println "Note Table: " note-table-ddl)
 
-(defonce db-tables (jdbc/execute! db-spec [user-table-ddl note-table-ddl]))
+; (jdbc/execute! db-spec [user-table-ddl])
+; (jdbc/execute! db-spec [note-table-ddl])
 
 (defn add-user [db table username email password] 
-    (j/insert db table {:username username :email email :password password}))
+    (jdbc/insert! db table {:username username :email email :password password}))
 
 (add-user db-spec :users "Jack_Sparrow" "pirate@bperl.com" "yohoho")
-
-(def ^:dynamic *note-state*
-    (atom {:typed-text ""
-           :notes  {0 {:id 0
-                       :text "Write Code."
-                       :done false
-                       :tags #{}}
-                    1 {:id 1
-                       :text "Drink coffee."
-                       :done true
-                       :tags #{}}}}))
